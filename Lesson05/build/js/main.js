1b7cc847defbfda823c37c21737aea7c7ffe21a2
Jaroslaw Charchula <charchula@gmail.com>
2023-04-27 10:55:30 +0200

    dom manipulation, double assertion, caution with assetions

--- Lesson05/build/js/main.js
+++ Lesson05/build/js/main.js
@@ -1,67 +1,23 @@
 "use strict";
-//Literal types
-let myName;
-let userName;
-userName = 'Amy';
-//Functions
-const add = (a, b) => {
-    return a + b;
+// conver to more or less specific
+let a = 'hello';
+let b = a; // less specific
+let c = a; // more specific
+let d = 'world'; // not rorking in tsx/react
+let e = 'world';
+const addOrConcat = (a, b, c) => {
+    if (c === 'add')
+        return a + b;
+    return '' + a + b;
 };
-const logMsg = (messege) => {
-    console.log(messege);
-};
-logMsg('Hello!!');
-logMsg(add(2, 3));
-let subtract = function (c, d) {
-    return c - d;
-};
-// interface mathFunction {
-//     (a: number, b: number): number
-// }
-let multiply = function (c, d) {
-    return c * d;
-};
-logMsg(multiply(2, 2));
-// optional parameters
-const addAll = (a, b, c) => {
-    if (typeof c !== 'undefined') {
-        return a + b + c;
-    }
-    return a + b;
-};
-// default param value
-const sumAll = (a = 10, b, c = 2) => {
-    return a + b + c;
-};
-logMsg(addAll(2, 3, 2));
-logMsg(addAll(2, 3));
-logMsg(sumAll(2, 3));
-logMsg(sumAll(undefined, 3));
-// Rest Parameters
-const total = (a, ...nums) => {
-    return a + nums.reduce((prev, curr) => prev + curr);
-};
-logMsg(total(10, 2, 3));
-const createError = (errMsg) => {
-    throw new Error(errMsg);
-};
-const infinite = () => {
-    let i = 1;
-    while (true) {
-        i++;
-        if (i > 100)
-            break;
-    }
-};
-// custom type guard
-const isNumber = (value) => {
-    return typeof value === 'number' ? true : false;
-};
-// use of the never type
-const numberOrString = (value) => {
-    if (typeof value === 'string')
-        return 'string';
-    if (isNumber(value))
-        return 'number';
-    return createError('This should never happen!');
-};
+let myVal = addOrConcat(2, 2, 'concat');
+// Be careful! TS sees no problem - but a string is returned
+let nextVal = addOrConcat(2, 2, 'concat');
+//avoid double assertion in general
+10;
+// The DOM
+const img = document.querySelector('img'); // not null assertion - ! at the end
+const myImg = document.getElementById('#img');
+const nextImg = document.getElementById('#img');
+img.src;
+myImg.src;
--- Lesson05/src/main.ts
+++ Lesson05/src/main.ts
@@ -1,95 +1,37 @@
-// Type Aliases
-type stringOrNumber = string | number;
+type One = string;
+type Two = string | number;
+type Three = 'hello';
 
-type stringOrNumberArr = (string | number)[];
+// conver to more or less specific
 
-type Guitarist = {
-  name?: string;
-  active: boolean;
-  albums: stringOrNumberArr;
+let a: One = 'hello';
+let b = a as Two; // less specific
+let c = a as Three; // more specific
+
+let d = <One>'world'; // not rorking in tsx/react
+let e = <string | number>'world';
+
+const addOrConcat = (
+  a: number,
+  b: number,
+  c: 'add' | 'concat'
+): number | string => {
+  if (c === 'add') return a + b;
+  return '' + a + b;
 };
 
-type UserId = stringOrNumber;
+let myVal: string = addOrConcat(2, 2, 'concat') as string;
 
-//Literal types
-let myName: 'Dave';
+// Be careful! TS sees no problem - but a string is returned
+let nextVal: number = addOrConcat(2, 2, 'concat') as number;
 
-let userName: 'Dave' | 'John' | 'Amy';
-userName = 'Amy';
+//avoid double assertion in general
+10 as unknown as string;
 
-//Functions
+// The DOM
+const img = document.querySelector('img')!; // not null assertion - ! at the end
+const myImg = document.getElementById('#img') as HTMLImageElement;
+const nextImg = <HTMLImageElement>document.getElementById('#img');
 
-const add = (a: number, b: number): number => {
-  return a + b;
-};
-
-const logMsg = (messege: any): void => {
-  console.log(messege);
-};
-
-logMsg('Hello!!');
-logMsg(add(2, 3));
-
-let subtract = function (c: number, d: number): number {
-  return c - d;
-};
-
-type mathFunction = (a: number, b: number) => number;
-// interface mathFunction {
-//     (a: number, b: number): number
-// }
-
-let multiply: mathFunction = function (c, d) {
-  return c * d;
-};
-
-logMsg(multiply(2, 2));
-
-// optional parameters
-const addAll = (a: number, b: number, c?: number): number => {
-  if (typeof c !== 'undefined') {
-    return a + b + c;
-  }
-  return a + b;
-};
-
-// default param value
-const sumAll = (a: number = 10, b: number, c: number = 2): number => {
-  return a + b + c;
-};
-
-logMsg(addAll(2, 3, 2));
-logMsg(addAll(2, 3));
-logMsg(sumAll(2, 3));
-logMsg(sumAll(undefined, 3));
-
-// Rest Parameters
-const total = (a: number, ...nums: number[]): number => {
-  return a + nums.reduce((prev, curr) => prev + curr);
-};
-
-logMsg(total(10, 2, 3));
-
-const createError = (errMsg: string): never => {
-  throw new Error(errMsg);
-};
-
-const infinite = () => {
-  let i: number = 1;
-  while (true) {
-    i++;
-    if (i > 100) break;
-  }
-};
-
-// custom type guard
-const isNumber = (value: any): boolean => {
-  return typeof value === 'number' ? true : false;
-};
-
-// use of the never type
-const numberOrString = (value: number | string): string => {
-  if (typeof value === 'string') return 'string';
-  if (isNumber(value)) return 'number';
-  return createError('This should never happen!');
-};
+img.src;
+myImg.src;
