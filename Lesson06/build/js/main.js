76ee8805c4154e30362cee6d665abccc9c519f89
Jaroslaw Charchula <charchula@gmail.com>
2023-04-29 01:39:48 +0200

    eidt Lesson06

--- Lesson06/build/js/main.js
+++ Lesson06/build/js/main.js
@@ -30,6 +30,56 @@ class WebDev extends Coder {
 }
 const Sara = new WebDev('Mac', 'Sara', 'Lofi', 25);
 console.log(Sara.getLang());
-// console.log(Sara.age)
-// console.log(Sara.lang)
-/////////////////////////////////////
+class Guitarist {
+    constructor(name, instrument) {
+        this.name = name;
+        this.instrument = instrument;
+    }
+    play(action) {
+        return `${this.name} ${action} the ${this.instrument}`;
+    }
+}
+const Page = new Guitarist('Jimmy', 'guitar');
+console.log(Page.play('strums'));
+//////////////////////////////////////
+class Peeps {
+    static getCount() {
+        return Peeps.count;
+    }
+    constructor(name) {
+        this.name = name;
+        this.name = name;
+        this.id = ++Peeps.count;
+    }
+}
+Peeps.count = 0;
+const John = new Peeps('John');
+const Steve = new Peeps('Steve');
+const Amy = new Peeps('Amy');
+console.log(Amy.id);
+console.log(Steve.id);
+console.log(John.id);
+console.log(Peeps.count);
+//////////////////////////////////
+class Bands {
+    constructor() {
+        this.dataState = [];
+    }
+    get data() {
+        return this.dataState;
+    }
+    set data(value) {
+        if (Array.isArray(value) && value.every((el) => typeof el === 'string')) {
+            this.dataState = value;
+            return;
+        }
+        else
+            throw new Error('Param is not an array of strings');
+    }
+}
+const MyBands = new Bands();
+MyBands.data = ['Neil Young', 'Led Zep'];
+console.log(MyBands.data);
+MyBands.data = [...MyBands.data, 'ZZ Top'];
+console.log(MyBands.data);
+//MyBands.data = ['Van Halen', 5150]; // must be string data
