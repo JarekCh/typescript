4328554411bcb1f7fca593b5b72c3cca6058f814
Jaroslaw Charchula <charchula@gmail.com>
2023-05-03 03:50:33 +0200

    add exercise Record, Required and Readonly

--- Lesson09/build/js/main.js
+++ Lesson09/build/js/main.js
@@ -10,3 +10,26 @@ const assign1 = {
 };
 console.log(updateAssignment(assign1, { grade: 95 }));
 const assignGraded = updateAssignment(assign1, { grade: 95 });
+// Required and Readonly
+const recordAssignment = (assign) => {
+    // send to database, etc.
+    return assign;
+};
+const assignVerified = Object.assign(Object.assign({}, assignGraded), { verified: true });
+// NOTE: assignVerified won't work with recordAssignment!
+// Why? Try it and see what TS tells you :)
+recordAssignment(Object.assign(Object.assign({}, assignGraded), { verified: true }));
+// Record
+const hexColorMap = {
+    red: 'FF0000',
+    green: '00FF00',
+    blue: '0000FF',
+};
+const finalGrades = {
+    Sara: 'B',
+    Kelly: 'U',
+};
+const gradeData = {
+    Sara: { assign1: 85, assign2: 93 },
+    Kelly: { assign1: 76, assign2: 15 },
+};
